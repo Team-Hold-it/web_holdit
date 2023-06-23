6bbf1f9150677956a02134a5e1d6528e312c649a
Peregrine9363 <peregrine9363@gmail.com>
2023-06-23 10:18:27 +0900

    다운로드 경로 변경

--- CCTV.py
+++ CCTV.py
@@ -3,7 +3,7 @@ import pandas as pd
 import subprocess
 
 # cctv url불러오기
-df = pd.read_csv("D:\Study\TeamProject_2\CCTV_API_Test\CCTVID_URL", index_col=0)
+df = pd.read_csv("CCTV_API_Test\CCTVID_URL", index_col=0)
 
 # CCTV.py
 def get_all_cctv_names():
@@ -16,6 +16,6 @@ def get_stream_url(cctv_name):
 # save_stream 함수 정의
 def save_stream(cctv_name, duration, output_filename):
     stream_url = get_stream_url(cctv_name)
-    output_folder = 'D:/Study/TeamProject_2/CCTV_API_Test/web_test_data/'
+    output_folder = 'CCTV_API_Test\web_test_data\\'
     command = f"ffmpeg -i \"{stream_url}\" -t {duration} -c copy \"{output_folder}{output_filename}\""
     subprocess.run(command, shell=True)
